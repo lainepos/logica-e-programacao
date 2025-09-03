f8cde9839c86813c376daf446bf435c50a481073
laine <lainepos@gmail.com>
2025-09-02 23:06:29 -0300

    Adicionado testes da função somar dois numeros

---
+++ test/calculadora.test.js
@@ -0,0 +1,23 @@
+const { somarDoisNumeros } = require('../src/calculadora');
+const { expect } = require ('chai');
+
+
+describe ('Testes da Função de Soma', function () {
+    it ('A função deve ser capaz de somar dois números positivos', function () {
+        // coleta o resultado da função
+        const resultadoDaSoma = somarDoisNumeros (5, 3);
+        // compara o resultado com o valor que você espera
+        expect(resultadoDaSoma).to.equal(8);
+    });
+
+
+    it ('A função deve ser capaz de somar um número positivo e um número negativo', function () {
+        const resultadoDaSoma = somarDoisNumeros (50, -15);
+        // compara o resultado com o valor que você espera
+        expect(resultadoDaSoma).to.equal(35);
+
+
+    });
+
+
+});
