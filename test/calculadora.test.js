e0c7fd4fddaf8c2f04a50d0e99ca54d888854fea
laine <lainepos@gmail.com>
2025-09-03 22:37:30 -0300

    Adicionando testes a função de somar dois numeros

--- test/calculadora.test.js
+++ test/calculadora.test.js
@@ -16,6 +16,21 @@ describe ('Testes da Função de Soma', function () {
         // compara o resultado com o valor que você espera
         expect(resultadoDaSoma).to.equal(35);
 
+    });
+       
+    // Teste Somar dois números zerados, ou seja, 0 + 0 igual a 0
+
+    it ('A função deve ser responsavel por somar dois numeros zerados', function () {
+        const resultadoDaSoma = somarDoisNumeros (0, 0);
+        expect(resultadoDaSoma).to.equal(0);
+
+    });
+
+    // Somar dois números negativos, por exemplo, -15 e -20 que deve ser -35
+
+    it ('A função deve ser responsavel por somar dois numeros negativos', function () {
+    const resultadoDaSoma = somarDoisNumeros (-15, -20);
+    expect(resultadoDaSoma).to.equal(-35);
 
     });
 
